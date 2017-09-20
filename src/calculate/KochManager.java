13464a4f96cc13431d8e46f69416bfec928c9a38
Zahharov,Sven S <zahharov@hotmail.com>
2017-09-20 15:30:14 +0200

    Stap 6 completed versie bewaren!

--- src/calculate/KochManager.java
+++ src/calculate/KochManager.java
@@ -2,13 +2,15 @@ package calculate;
 
 import jsf31kochfractalfx.JSF31KochFractalFX;
 
+import timeutil.TimeStamp;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class KochManager implements Observer {
     KochFractal koch = new KochFractal();
     private JSF31KochFractalFX application;
-
+    private ArrayList<Edge> edges = new ArrayList<>();
     public KochManager(JSF31KochFractalFX application) {
         this.application = application;
         koch.addObserver(this);
@@ -19,12 +21,22 @@ public class KochManager implements Observer {
     }
     public void drawEdges() {
         application.clearKochPanel();
+
+        TimeStamp stamp = new TimeStamp();
+
+        stamp.setBegin("Start calculation");
         koch.generateLeftEdge();
         koch.generateBottomEdge();
         koch.generateRightEdge();
+        for(Edge edge : edges)
+            application.drawEdge(edge);
+        stamp.setEnd("calculation complete");
+        application.setTextCalc(stamp.toString());
+        application.setTextNrEdges(""+edges.size());
+
     }
     @Override
     public void update(Observable o, Object arg) {
-        application.drawEdge((Edge)arg);
+        edges.add((Edge)arg);
     }
 }
